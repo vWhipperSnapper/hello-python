bde094e42b3c49fdee8299010f869590acfc38a1
Matt Cowger <matt@cowger.us>
2014-04-14 15:49:22 -0700

    Basic

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
 import os
+import pprint
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/')
 def hello():
-	return 'Hello World!'
+	return 'Hello World!\n'
 
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
