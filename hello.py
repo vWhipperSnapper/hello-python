b7f578658de2a9af082d1da8d3101e838ec4f446
Marques <marquj5@usxxmarquj5m2.corp.emc.com>
2015-09-09 15:20:06 -0400

    added images code to source code JM

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+import Image
 from flask import Flask
 
 
@@ -27,6 +28,17 @@ def hello():
     </body>
     </html>
     """.format(COLOR,my_uuid,)
+    
+    {}</br>
+
+if COLOR==GREEN
+    	</center>
+    	image=Image.open('/Users/marquj5/hello-python/tmnt.png').show()
+
+if COLOR==BLUE
+	</center>
+image=Image.open('/Users/marquj5/hello-python/spongebob.jpg').show()
+
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
