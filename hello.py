8153b39e953a19d7ec9a0187891a343b602bf502
Marques <marquj5@usxxmarquj5m2.corp.emc.com>
2015-09-09 18:14:24 -0400

    Final image tmnt

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 import os
 import uuid
-import Image
 from flask import Flask
 
 
@@ -14,6 +13,8 @@ COLOR = GREEN
 @app.route('/')
 def hello():
 
+    if COLOR==GREEN:
+        myimage='<img src="http://teenagemutantninjaturtles.com/wp-content/uploads/2013/03/Ninja-Turtles-TMNT-Pizza.jpg">'
 
     return """
     <html>
@@ -22,23 +23,15 @@ def hello():
     <center><h1><font color="white">Hi, I'm GUID:<br/>
     {}</br>
 
-
+    {}
     </center>
 
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,myimage)
+
+
     
-    
-
-if COLOR==GREEN
-    	</center>
-    	image=Image.open('/Users/marquj5/hello-python/tmnt.png').show()
-
-if COLOR==BLUE
-	</center>
-image=Image.open('/Users/marquj5/hello-python/spongebob.jpg').show()
-
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
