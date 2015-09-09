d27f0024072aa834d1db9a2c777fdc22cc46ca20
Matt Cowger <matt@cowger.us>
2015-09-09 11:11:31 -0700

    Update

--- hello.py
+++ hello.py
@@ -1,28 +1,32 @@
 import os
 import uuid
 from flask import Flask
+
+
 app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
 
-COLOR = BLUE
+COLOR = GREEN
 
 @app.route('/')
 def hello():
-	
-	return """
-	<html>
-	<body bgcolor="{}">
 
-	<center><h1><font color="white">Hi, I'm GUID:<br/>
-	{}</br>
-	
-	</center>
 
-	</body>
-	</html>
-	""".format(COLOR,my_uuid)
+    return """
+    <html>
+    <body bgcolor="{}">
+
+    <center><h1><font color="white">Hi, I'm GUID:<br/>
+    {}</br>
+
+
+    </center>
+
+    </body>
+    </html>
+    """.format(COLOR,my_uuid,)
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
